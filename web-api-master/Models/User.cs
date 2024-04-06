14ce3f875df71bbca86f0d03d0f9d2f426d9e588
Amelija77 <chamelijonas77@gmail.com>
2024-04-06 16:26:20 +0200

    Front-end main config

---
+++ web-api-master/Models/User.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mail;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScanAndGoApi.Models
+{
+    [Table("USER")]
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public long id { get; set; }
+
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+
+  
+        [Required] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+
+        [JsonIgnore]
+        public ShoppingList? ShoppingList { get; set; }
+    }
+}
